6c4baf8987562bea0a399a0314ddc114255031c2
Roy Tu <thatsnail@gmail.com>
2014-06-28 01:03:36 -0400

    Better testing environment

--- tests.py
+++ tests.py
@@ -3,18 +3,24 @@ from onode import ONode
 from network import Network
 
 class Tests(object):
-    def __init__(self):
-        inputs = [INode(x) for x in [0, 1]]
+    def printResult(self, expected, actual):
+        print("Expected:\n" + expected)
+        print("\n")
+        print("Actual:\n" + str(actual))
+        print("\n")
+
+    def testBackpropagation(self):
+        inputs = [INode(state=0), INode(state=1)]
         outputs = [ONode(expectationValue=0)]
         for i in inputs:
             for o in outputs:
-                i.link(o, 0)
+                i.link(o, weight=0)
         net = Network(inputs, outputs)
-        print(net)
-        net.fireAll()
-        print(net)
-        net.backpropagate()
-        print(net)
+
+        self.printResult("Input: [  0  1  ]\nOutput: [  0  ]", net)
+
+    def __init__(self):
+        self.testBackpropagation()
 
 if __name__ == "__main__":
     Tests()
