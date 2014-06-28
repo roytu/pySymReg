c5f88d7f27631d0f5a23a1e9bfd7683179b286b8
Roy Tu <thatsnail@gmail.com>
2014-06-28 01:31:36 -0400

    Basic backpropagation tests

--- tests.py
+++ tests.py
@@ -5,9 +5,8 @@ from network import Network
 class Tests(object):
     def printResult(self, expected, actual):
         print("Expected:\n" + expected)
-        print("\n")
         print("Actual:\n" + str(actual))
-        print("\n")
+        print("")
 
     def testBackpropagation(self):
         inputs = [INode(state=0), INode(state=1)]
@@ -20,6 +19,14 @@ class Tests(object):
         self.printResult("Input: [  0  1  ]\nOutput: [  0  ]", net)
         net.fireAll()
         self.printResult("Input: [  0  1  ]\nOutput: [  0.5  ]", net)
+        net.backpropagate(0.9)
+        self.printResult("-0.11750185610079725", outputs[0].gradient)
+        self.printResult("0", inputs[0].gradient)
+        self.printResult("0", inputs[1].gradient)
+        self.printResult("0", outputs[0].links[inputs[0]].getWeight())
+        self.printResult("-0.10575167049071753", outputs[0].links[inputs[1]].getWeight())
+        self.printResult("0", inputs[0].links[outputs[0]].getWeight())
+        self.printResult("-0.10575167049071753", inputs[1].links[outputs[0]].getWeight())
 
     def __init__(self):
         self.testBackpropagation()
