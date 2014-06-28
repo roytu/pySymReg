8a05d01da929b58f805b125b5451eddb65b0c456
Roy Tu <thatsnail@gmail.com>
2014-06-28 02:09:35 -0400

    Added boolean logic test (OR)

---
+++ tests/backpropagation.py
@@ -0,0 +1,25 @@
+from inode import INode
+from onode import ONode
+from network import Network
+
+from test_utils import printResult
+
+def testBackpropagation():
+    inputs = [INode(state=0), INode(state=1)]
+    outputs = [ONode(expectationValue=0)]
+    for i in inputs:
+        for o in outputs:
+            i.link(o, weight=0)
+    net = Network(inputs, outputs)
+
+    printResult("Input: [  0  1  ]\nOutput: [  0  ]", net)
+    net.fireAll()
+    printResult("Input: [  0  1  ]\nOutput: [  0.5  ]", net)
+    net.backpropagate(0.9)
+    printResult("-0.11750185610079725", outputs[0].gradient)
+    printResult("0", inputs[0].gradient)
+    printResult("0", inputs[1].gradient)
+    printResult("0", outputs[0].links[inputs[0]].getWeight())
+    printResult("-0.10575167049071753", outputs[0].links[inputs[1]].getWeight())
+    printResult("0", inputs[0].links[outputs[0]].getWeight())
+    printResult("-0.10575167049071753", inputs[1].links[outputs[0]].getWeight())
---
+++ tests/boolean_logic.py
@@ -0,0 +1,40 @@
+from inode import INode
+from onode import ONode
+from network import Network
+
+from test_utils import printResult
+
+def testBooleanLogic():
+    # OR
+    inputs = [INode(state=0), INode(state=0)]
+    outputs = [ONode(expectationValue=0)]
+    for i in inputs:
+        for o in outputs:
+            i.link(o, weight=1)
+    net = Network(inputs, outputs)
+
+    for _ in range(10):
+        for (i0, i1, ex) in [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1)]:
+            inputs[0].setState(i0)
+            inputs[1].setState(i1)
+            outputs[0].setExpectation(ex)
+            net.fireAll()
+            net.backpropagate(0.9)
+
+    # Error rates
+    inputs[0].setState(0)
+    inputs[1].setState(0)
+    net.fireAll()
+    printResult("<0.5", outputs[0])
+    inputs[0].setState(0)
+    inputs[1].setState(1)
+    net.fireAll()
+    printResult(">0.5", outputs[0])
+    inputs[0].setState(1)
+    inputs[1].setState(0)
+    net.fireAll()
+    printResult(">0.5", outputs[0])
+    inputs[0].setState(1)
+    inputs[1].setState(1)
+    net.fireAll()
+    printResult(">0.5", outputs[0])
