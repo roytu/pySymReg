218333de37bf1a468f343b66b8ba7e6df2833d89
Roy Tu <thatsnail@gmail.com>
2014-06-27 23:17:09 -0400

    Added expectation value for output nodes

--- onode.py
+++ onode.py
@@ -1,5 +1,9 @@
 from node import Node
 
 class ONode(Node):
+    def __init__(self, *args, expectationValue=None):
+        Node.__init__(self, *args)
+        self.expectationValue = expectationValue
+
     def getState(self):
         return self.state
