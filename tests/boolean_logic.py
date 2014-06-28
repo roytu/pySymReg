746cfdc85076db5ebc23bcc47d22ad42a090bf37
Roy Tu <thatsnail@gmail.com>
2014-06-28 14:17:09 -0400

    Reformatted to use testNetwork

--- tests/boolean_logic.py
+++ tests/boolean_logic.py
@@ -1,124 +1,28 @@
-from inode import INode
-from onode import ONode
-from node import Node
-from network import Network
-
-from test_utils import testResult
+from test_utils import testResult, testNetwork
 
 def testOR():
-    inputs = [INode(state=0), INode(state=0)]
-    outputs = [ONode(expectationValue=0)]
-    for i in inputs:
-        for o in outputs:
-            i.link(o, weight=1)
-    net = Network(inputs, outputs)
-
-    for _ in range(10):
-        for (i0, i1, ex) in [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 1)]:
-            inputs[0].setState(i0)
-            inputs[1].setState(i1)
-            outputs[0].setExpectation(ex)
-            net.fireAll()
-            net.backpropagate(0.9)
-
-    # Error rates
-    inputs[0].setState(0)
-    inputs[1].setState(0)
-    net.fireAll()
-    testResult(outputs[0].state < 0.5, "OR Gate, with inputs 0 0")
-
-    inputs[0].setState(0)
-    inputs[1].setState(1)
-    net.fireAll()
-    testResult(outputs[0].state > 0.5, "OR Gate, with inputs 0 1")
-
-    inputs[0].setState(1)
-    inputs[1].setState(0)
-    net.fireAll()
-    testResult(outputs[0].state > 0.5, "OR Gate, with inputs 1 0")
-
-    inputs[0].setState(1)
-    inputs[1].setState(1)
-    net.fireAll()
-    testResult(outputs[0].state > 0.5, "OR Gate, with inputs 1 1")
+    testNetwork(2, [], 1, [
+                            ([0, 0], [0], [lambda x: x < 0.5], "OR Gate, with inputs 0 0"),
+                            ([0, 1], [1], [lambda x: x > 0.5], "OR Gate, with inputs 0 1"),
+                            ([1, 0], [1], [lambda x: x > 0.5], "OR Gate, with inputs 1 0"),
+                            ([1, 1], [1], [lambda x: x > 0.5], "OR Gate, with inputs 1 1")
+                         ])
 
 def testAND():
-    inputs = [INode(state=0), INode(state=0)]
-    outputs = [ONode(expectationValue=0)]
-    for i in inputs:
-        for o in outputs:
-            i.link(o, weight=1)
-    net = Network(inputs, outputs)
-
-    for _ in range(10):
-        for (i0, i1, ex) in [(0, 0, 0), (0, 1, 0), (1, 0, 0), (1, 1, 1)]:
-            inputs[0].setState(i0)
-            inputs[1].setState(i1)
-            outputs[0].setExpectation(ex)
-            net.fireAll()
-            net.backpropagate(0.9)
-
-    # Error rates
-    inputs[0].setState(0)
-    inputs[1].setState(0)
-    net.fireAll()
-    testResult(outputs[0].state < 0.5, "AND Gate, with inputs 0 0")
-
-    inputs[0].setState(0)
-    inputs[1].setState(1)
-    net.fireAll()
-    testResult(outputs[0].state < 0.5, "AND Gate, with inputs 0 1")
-
-    inputs[0].setState(1)
-    inputs[1].setState(0)
-    net.fireAll()
-    testResult(outputs[0].state < 0.5, "AND Gate, with inputs 1 0")
-
-    inputs[0].setState(1)
-    inputs[1].setState(1)
-    net.fireAll()
-    testResult(outputs[0].state > 0.5, "AND Gate, with inputs 1 1")
+    testNetwork(2, [], 1, [
+                            ([0, 0], [0], [lambda x: x < 0.5], "AND Gate, with inputs 0 0"),
+                            ([0, 1], [0], [lambda x: x < 0.5], "AND Gate, with inputs 0 1"),
+                            ([1, 0], [0], [lambda x: x < 0.5], "AND Gate, with inputs 1 0"),
+                            ([1, 1], [1], [lambda x: x > 0.5], "AND Gate, with inputs 1 1")
+                         ])
 
 def testXOR():
-    inputs = [INode(state=0), INode(state=0)]
-    hidden = [Node(), Node()]
-    outputs = [ONode(expectationValue=0)]
-    for i in inputs:
-        for h in hidden:
-            i.link(h, weight=1)
-    for o in outputs:
-        for h in hidden:
-            h.link(o, weight=1)
-    net = Network(inputs, outputs)
-
-    for _ in range(10):
-        for (i0, i1, ex) in [(0, 0, 0), (0, 1, 1), (1, 0, 1), (1, 1, 0)]:
-            inputs[0].setState(i0)
-            inputs[1].setState(i1)
-            outputs[0].setExpectation(ex)
-            net.fireAll()
-            net.backpropagate(0.9)
-
-    # Error rates
-    inputs[0].setState(0)
-    inputs[1].setState(0)
-    net.fireAll()
-    testResult(outputs[0].state < 0.5, "XOR Gate, with inputs 0 0")
-
-    inputs[0].setState(0)
-    inputs[1].setState(1)
-    net.fireAll()
-    testResult(outputs[0].state > 0.5, "XOR Gate, with inputs 0 1")
-
-    inputs[0].setState(1)
-    inputs[1].setState(0)
-    net.fireAll()
-    testResult(outputs[0].state > 0.5, "XOR Gate, with inputs 1 0")
-
-    inputs[0].setState(1)
-    inputs[1].setState(1)
-    net.fireAll()
-    testResult(outputs[0].state < 0.5, "XOR Gate, with inputs 1 1")
+    testNetwork(2, [], 1, [
+                            ([0, 0], [0], [lambda x: x < 0.5], "XOR Gate, with inputs 0 0"),
+                            ([0, 1], [1], [lambda x: x > 0.5], "XOR Gate, with inputs 0 1"),
+                            ([1, 0], [1], [lambda x: x > 0.5], "XOR Gate, with inputs 1 0"),
+                            ([1, 1], [0], [lambda x: x < 0.5], "XOR Gate, with inputs 1 1")
+                         ])
 
 def testBooleanLogic():
     testOR()
