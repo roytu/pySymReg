aa563eabbde5ca308afb8432285a0c7d94efbfe6
Roy Tu <thatsnail@gmail.com>
2014-07-03 04:58:21 -0400

    Added step function

--- activation_functions.py
+++ activation_functions.py
@@ -11,3 +11,9 @@ def fnTanhShifted():
     f = lambda x: tanh(x) / 2 + 0.5
     df = lambda x: pow(1 / cosh(x), 2) / 2
     return (f, df)
+
+def fnStep():
+    """ Step function. Should not be used for propagation """
+    f = lambda x: 0 if x < 0 else 1
+    df = lambda _: 0
+    return (f, df)
